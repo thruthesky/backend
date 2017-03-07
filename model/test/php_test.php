be6bf98f237b9cb2ed0a8e4173f5c4496ae059aa
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-03-07 15:53:59 +0800

    adding more test on meta, user, php

--- model/test/php_test.php
+++ model/test/php_test.php
@@ -8,8 +8,14 @@ class Php_Test {
     }
     public function run()
     {
-        $meta = meta();
-        test( $meta instanceof \model\meta\Meta, "Meta is Meta" );
+        $user = user();
+        test( $user instanceof \model\user\User, "User is User" );
+
+        $admin = user('admin');
+        test( $admin instanceof \model\taxonomy\Taxonomy, "User is a child of Taxonomy");
+        test( $admin instanceof \model\entity\Entity, "User is a child of Entity");
+        test( $admin instanceof \model\user\User, "And user is User");
+
     }
 }
 
