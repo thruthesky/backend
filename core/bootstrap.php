927a27b4cc9b75a018a1a6352a04c7465fd8f18b
JaeHo Song <thruthesky@gmail.com>
2017-04-03 15:50:16 +0800

    pupdate on bootstrap

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,12 +4,12 @@ $system = []; // system variable used for holding system data.
 
 if ($_SERVER['REQUEST_METHOD']=='OPTIONS') {
 
-    header("Access-Control-Allow-Origin : $_SERVER[HTTP_ORIGIN]");
+    header("Access-Control-Allow-Origin : abc.com");
     header('Access-Control-Allow-Methods : POST, GET, OPTIONS, PUT, DELETE');
     header('Access-Control-Allow-Headers : X-Requested-With, content-type');
     exit;
 }
-header('Access-Control-Allow-Origin: *');	/** For ajax json calling from outside */
+header('Access-Control-Allow-Origin: abc.com');	/** For ajax json calling from outside */
 header('P3P: CP="NOI ADM DEV COM NAV OUR STP"'); /** cookie share on iframe */
 
 
