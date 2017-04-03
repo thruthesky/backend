1311dba3150160fa4698214efd1ed16ea72bbe1a
lancelynyrd <pinedaclp@gmail.com>
2017-04-03 16:31:28 +0800

    local settings on my_config

--- etc/my_config.php
+++ etc/my_config.php
@@ -1,11 +1,19 @@
 <?php
 
+//   git update-index --assume-unchanged etc/my_config.php
+//   git update-index --no-assume-unchanged etc/my_config.php
+
+//$ADMIN_ID               = 'admin';          // this is admin id.
+//$ANONYMOUS_ID          = 'anonymous';      // Anonymous ID.
 
 
-// $DATABASE_USER          = 'root';
-// $DATABASE_PASSWORD      = '7777';
-$DATABASE_NAME          = 'backend';
-// $DATABASE_HOST          = 'localhost';
 
-$DATABASE_TYPE          = 'mysql';
+//$DIR_DATA               = './data';
+
+$DATABASE_USER          = 'roots';
+$DATABASE_PASSWORD      = '7777';
+//$DATABASE_NAME          = 'backend';
+//$DATABASE_PREFIX        = "be_";
+//$DATABASE_HOST          = 'localhost';
+$DATABASE_TYPE          = 'mysql';         // 'mysql' | 'sqlite'
 
