dc412fde779b6660c6b057e09194aed126ee4382
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-04-29 21:06:09 +0800

    update for admin page

--- view/admin/src/app/app.module.ts
+++ view/admin/src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+
 
 import { AppComponent } from './app.component';
+import { HomePage } from './../pages/home/home';
+
+import { HeaderComponent } from './../components/header/header';
+
+
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomePage, pathMatch: 'full' },
+  { path: '**', component: HomePage }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePage,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot( appRoutes )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ view/admin/src/components/header/header.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'header-component',
+    templateUrl: 'header.html'
+})
+
+export class HeaderComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
