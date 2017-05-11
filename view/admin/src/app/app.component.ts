dc6948a9f5b48d272f2988162913b66b263e6cb2
JaeHo Song <thruthesky@gmail.com>
2017-05-11 20:50:09 +0800

    fix api

--- view/admin/src/app/app.component.ts
+++ view/admin/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Backend } from "angular-backend";
+import { environment } from './../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +10,8 @@ export class AppComponent {
   constructor(
     private backend: Backend
   ){
-    this.backend.setBackendUrl("http://backend.org/index.php");
+    this.backend.setBackendUrl( environment.backendApi );
+    console.log("backend api: ", environment.backendApi );
   }
 
 }
