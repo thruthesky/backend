710511a4400c06be7a912036e4508fcab84e24fc
JaeHo Song <thruthesky@gmail.com>
2017-05-15 16:37:39 +0800

    update for admim page

--- view/admin/src/app/components/header/header.ts
+++ view/admin/src/app/components/header/header.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User, _USER_LOGOUT_RESPONSE } from 'angular-backend';
 
+import { Observable, Subject } from 'rxjs';
+
+
 @Component({
     moduleId: module.id,
     selector: 'header-component',
@@ -12,10 +15,34 @@ export class HeaderComponent implements OnInit {
     constructor(
         public user: User,
         private router: Router
-    ) { }
+    ) {
+
+
+
+    //   var obs = Observable.create( (observer) => {
+    //        setTimeout(()=>{
+    //            observer.next("after timeout");
+    //        }, 100);
+    //        setTimeout(() => {
+    //            observer.error( new Error('oo'));
+    //        }, 1000);
+    //        observer.next( 'first value' );
+    //    } );
+    //    obs
+    //    .subscribe( res => {
+    //        console.log('res of obs: ', res);
+    //    }, e => {
+    //        console.log("Got error", e);
+    //    });
+
+
+
+
+
+    }
 
     onClickLogout() {
-        this.router.navigate( [ '/' ] );
+        this.router.navigate(['/']);
         this.user.logout().subscribe((res: _USER_LOGOUT_RESPONSE) => {
             console.log(res);
         }, err => {
