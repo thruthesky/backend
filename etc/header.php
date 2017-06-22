1d8ff0f88eb9c128e67aecfd5d493a88ee343a5d
JaeHo song <thruthesky@gmail.com>
2017-06-22 16:51:38 +0000

    update for config

--- etc/header.php
+++ etc/header.php
@@ -1,7 +1,7 @@
 <?php
     $host = $_SERVER['HTTP_HOST'];
     if ( strpos( $host, 'katalkenglish.com' ) !== false ) $naver_login_client_id = "RQ5aql3x2TQKjwi2Pkh4";
-    else if ( strpos( $host, 'katalkenglish.com' ) !== false ) $naver_login_client_id = "hLsDccGynf0Kci1AxLjL";
+    else if ( strpos( $host, 'iamtalkative.com' ) !== false ) $naver_login_client_id = "hLsDccGynf0Kci1AxLjL";
     else $naver_login_client_id = "RQ5aql3x2TQKjwi2Pkh4";
 ?>
 <script>
